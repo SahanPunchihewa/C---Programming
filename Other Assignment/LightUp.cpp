a05703eff0deffc98973d405d76fdbc798053480
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-01-07 19:53:51 +0530

    Assignment

--- Other Assignment/LightUp.cpp
+++ Other Assignment/LightUp.cpp
@@ -1,66 +1,156 @@
-#include<iostream>
-#include<string.h>
-using namespace std;
+#include<iostream> // iostream Headerfile
+#include<string> // String Headerfile
+using namespace std; // pre-fix for std
 
-class Login {
+class Login { // Login Class
+ 
+    public : // Public Properties
 
-    private : 
+    string UserNameAttempt; 
+    string PasswordAttempt;
+    Login() {
 
-        string UserName = "User";
-        string Password = "Password";
-
-     public :
-
-     Login();
-     Login(string UserNameAttempt, string PasswordAttempt);
-     void UserLogin();
-     
-};
-
-Login ::Login() {
-
-    UserName = "";
-    Password = "";
-
-    // Default Constructor Called
-}
-
-Login ::Login(string UserNameAttempt, string PasswordAttempt) {
-
-    UserName = UserNameAttempt;
-    Password = PasswordAttempt;
-
-    // Overload Constructor called
-
-}
-
-void Login :: UserLogin() {
-
-    Cout<< "Please Enter Your User Name :";
-    cin >> UserName;
-
-    if(UserName == UserNameAttempt) {
-
-    cout << "Please Enter Your Password :";
-    cin >> Password;
-
-        if(Password == PasswordAttempt) {
-
-            cout << "Welcome to LightUp Electricity Cop "
-        }
+        accessGranted = 0;
 
     }
 
-}
+    void LoginUser() { // Methods
 
-int main() {
+        cout << "Welcome to LightUp Electricity Coporation...!!!" << endl;
+        cout << "-----------------------------------------------" << endl;
+        cout <<endl;
 
-    Login login;
+        cout << "Please Enter Your User Name :";
+        cin >> UserNameAttempt;
+            if(UserNameAttempt == UserName) {
 
-    
+                    cout << "Please Enter Your Password :";
+                    cin >> PasswordAttempt;
+                    if(PasswordAttempt == Password) {
+
+                        cout << "Login Successful...!!!" << endl;
+                    }
+                    else {
+
+                        cout << "Login Failed...!!!" << endl;
+                    }
+     } 
+        
+    }
+
+    private : // Private Properties
+
+        string Password = "Password";
+        string UserName = "UserName";
+        bool accessGranted;
+
+
+};
+
+class Register { // Class Register
+
+    private : // Private Properties
+
+    string AccountNumber;
+    string Name;
+    string Address;
+    int TelephoneNumber;
+
+    public : // Public Properties
+
+    Register() {
+
+        AccountNumber = "";
+        Name = "";
+        Address = "";
+        TelephoneNumber = 0;
+
+    }
+    Register(string accNumber, string name, string address, int telephone) {
+
+        AccountNumber = accNumber;
+        Name = name;
+        Address = address;
+        TelephoneNumber = telephone;
+    }
+
+    void UserRegister() { // Methhos
+
+        cout <<"User Registration" << endl;
+        cout << "--------------------------------" << endl;
+        cout <<endl;
+
+        cout << "Enter Your Account Number :";
+        cin >> AccountNumber;
+
+        cout << "Enter Your Name :";
+        cin >> Name;
+
+        cout <<"Enter Your Address :";
+        cin >> Address;
+
+        cout << "Enter Your Telephone Number :";
+        cin >> TelephoneNumber;
+        cout << endl;
+        cout << endl;
+
+        cout << "Successfully Registered...!!!" << endl;
+
+
+    }
+
+    void Display(){ // methods
+
+        cout << "Account Number :" << AccountNumber << endl;
+        cout << "Name :" << Name << endl;
+        cout << "Address :" << Address << endl;
+        cout << "Telephone Number :" << TelephoneNumber << endl;
+
+    }
+
+};
+
+int main() { // Main Method
+
+    char choice; // Variable
+
+    Login l;
+    l.LoginUser(); // Method call
+
+    cout << "Do you want Continue? (Y/N)"; // Continue or not
+    cin >> choice;
+    cout << endl;
+
+    if(choice == 'Y' || choice == 'y') { // condition
+
+        Register r;
+        r.UserRegister();
+        r.Display();
+
+    }
+    else { // condition
+
+        cout << "Thank You...!!!" << endl;
+    }
+
+    cout << "Do you want Display Details? (Y/N)"; // Display Details or not
+    cin >> choice;
+    cout << endl;
+
+     if(choice == 'Y' || choice == 'y') { // condition
+
+        Register r;
+        r.Display(); // Method Call
+
+    }
+    else {
+
+        cout << "Thank You...!!!" << endl;
+    }
 
 
 
+    return 0; // End of Main Method
 
-    return 0;
+
 }
